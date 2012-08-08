9e4f2761c2dc7213e980bdbc175042942c4a8eaf
JoelPacheco <joelprogramacion@gmail.com>
2012-08-08 01:33:18 -0500

    algunos cambios mientras aprendo a usar mockito
    algunos cambios mientras aprendo a usar mockito

--- InstitutoBenedictoXVI/src/dominio/Clientealta.java
+++ InstitutoBenedictoXVI/src/dominio/Clientealta.java
@@ -4,20 +4,23 @@ public class Clientealta {
 
 	public boolean ValidaDatos(String nombre, String ape_paterno,
 			String ape_materno, String email, String dni, String telefono,
-			String celular) {
+			String celular)  throws ParametroIncompleto{
 		
 		boolean validado = true;
 		
 		if (nombre==null) {
 			 validado = false;
+			 throw new ParametroIncompleto("No ingreso nombre");
 			
 		}
 		if (ape_paterno==null ) {
 			 validado = false;
+			 throw new ParametroIncompleto("No ingreso apellido paterno");
 		}
 		
 		if(email == null){
 			 validado = false;
+			 throw new ParametroIncompleto("No ingreso email");
 		}
 
 		return validado;
--- InstitutoBenedictoXVI/test/test/ClienteTest2.java
+++ InstitutoBenedictoXVI/test/test/ClienteTest2.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import dominio.Clientealta;
+import dominio.ParametroIncompleto;
 
 
 public class ClienteTest2 {
@@ -10,25 +11,29 @@ public class ClienteTest2 {
 	@Test
 	public void AltaDeClientes(){
 		
+		/*Los campos nombre,apellido paterno,email son obligatorios*/
+		
 		//String nombre = "joel";
-		String nombre = null;
+		String nombre = "Joel";
 		//String ape_paterno= "Pacheco";
-		String ape_paterno=null;
+		String ape_paterno="Pacheco";
 		String ape_materno= null;
 		//String email = "joel@gmail.com";
-		String email = null;
+		String email = "Jo@gmail.com";
 		String dni = null;
 		String telefono = null;
 		String celular = null;
 		
 		Clientealta alta = new Clientealta();
-		
-		
-		boolean datoscorrectos = alta.ValidaDatos(nombre,ape_paterno,ape_materno,email,dni,telefono,celular); 
-		
-		
-		
-		Assert.assertTrue("No a ingresado datos relevantes",datoscorrectos);
+
+		boolean datoscorrectos;
+		try {
+			datoscorrectos = alta.ValidaDatos(nombre,ape_paterno,ape_materno,email,dni,telefono,celular);
+			Assert.assertTrue("No a ingresado datos relevantes",datoscorrectos);
+		} catch (ParametroIncompleto e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
 	}
 
